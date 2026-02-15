25b1256d34aa9d44471f2e5b752e2335c83b90ed
AlencarLima <carlos.alencar.99@gmail.com>
2026-02-15 11:15:07 -0300

    feat: update PlanningView to use PaperCard for content display

--- apps/container/src/views/Planning/PlanningView.vue
+++ apps/container/src/views/Planning/PlanningView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="planning">
-    <h1>Planning</h1>
+    <PaperCard title="Planejamento" class="text-next-grey">
+      <p class="mt-4">
+        Essa página é dedicada para organizar suas disciplinas cursadas e ver
+        o progresso de sua graduação.
+      </p>
+    </PaperCard>
   </div>
 </template>
+
+<script setup lang="ts">
+import { PaperCard } from '@/components/PaperCard';
+</script>
