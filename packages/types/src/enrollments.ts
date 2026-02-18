df7224dfa976bcce74f4b442fb1cab1334e63beb
AlencarLima <carlos.alencar.99@gmail.com>
2026-02-18 12:10:21 -0300

    feat: add Discipline interface and TPEI type to enhance enrollment structure

--- packages/types/src/enrollments.ts
+++ packages/types/src/enrollments.ts
@@ -44,8 +44,34 @@ export type Enrollment = {
   conceito: Concept;
   creditos: number;
   disciplina: string;
+  code: string;
   quad: number;
   subject: Subject;
   year: number;
   comments?: string[];
 };
+
+interface DisciplineType {
+  name: string;
+  quarter: number | null;
+}
+
+// Formato observado: "0-3-3-2"
+export type TPEI = `${number}-${number}-${number}-${number}`;
+
+export interface Discipline {
+  id: number;
+  code: string;
+  name: string;
+  credits: number;
+  period?: number | string; // pode ser "" em alguns casos, então mantenha como string
+  disciplineCategory: DisciplineType[];
+  tpei?: string;
+  recommendedSubjects?: string;
+  // objective é opcional
+  objectives?: string;
+  syllabus?: string;
+  extensionMethodology?: string;
+  basicBibliography?: string; // está como texto com quebras de linha
+  complementaryBibliography?: string; // idem
+}
